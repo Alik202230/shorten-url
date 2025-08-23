ab9ffd194cc47083603a03ec675eed9419278e64
Alik Sargsyan <alik.sargsyan@coderepablic.am>
2025-08-23 18:57:32 +0400

    remove method

--- src/main/java/am/itspace/shortest/url/repository/ShortUrlRepository.java
+++ src/main/java/am/itspace/shortest/url/repository/ShortUrlRepository.java
@@ -15,8 +15,4 @@ public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
 
   Optional<ShortUrl> findByOriginalUrl(String originalUrl);
 
-  @Modifying
-  @Query("update ShortUrl s set s.isActive = true, s.clickCount = s.clickCount + 1 where s.shortKey = :shortKey")
-  void incrementClickCount(@Param("shortKey") String shortKey);
-
 }
